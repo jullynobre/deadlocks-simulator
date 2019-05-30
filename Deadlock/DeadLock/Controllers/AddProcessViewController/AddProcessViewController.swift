ca5cdd666d433c45fbc114a4becc9ae3ed544210
jullynobre <jullynobre1@gmail.com>
2019-05-30 18:07:21 -0300

    Add ProcessAddedProtocol

--- Deadlock/DeadLock/Controllers/AddProcessViewController/AddProcessViewController.swift
+++ Deadlock/DeadLock/Controllers/AddProcessViewController/AddProcessViewController.swift
@@ -8,14 +8,31 @@
 
 import Cocoa
 
+protocol ProcessAdded: class {
+	func didAddedProcess(_ sender: AddProcessViewController,
+						 processId: Int,
+						 processTs: Int,
+						 processTu: Int)
+}
+
 class AddProcessViewController: NSViewController {
 
-    override func viewDidLoad() {
+	@IBOutlet weak var processIdTextField: NSTextField!
+	@IBOutlet weak var processTuTextField: NSTextField!
+	@IBOutlet weak var processTsTextField: NSTextField!
+	
+	weak var delegate: ProcessAdded?
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
     }
-	@IBAction func didTapSaveButton(_ sender: Any) {
 	
+	@IBAction func didTapSaveButton(_ sender: Any) {
+		delegate?.didAddedProcess(self, processId: Int(self.processIdTextField!.stringValue) ?? 0,
+								  processTs: Int(self.processTsTextField!.stringValue) ?? 0,
+								  processTu: Int(self.processTuTextField!.stringValue) ?? 0)
+		self.dismiss(nil)
 	}
 	
 	@IBAction func didTapCancelButton(_ sender: Any) {
